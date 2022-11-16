bd502e93204085835c75a426875a9bbd75afcbc8
HEMA <HEMA@LAPTOP-KT7T3VB4>
2022-11-16 15:19:49 +0530

    Add project files.

---
+++ vs-2022-github-User-Experience1/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace vs_2022_github_User_Experience1
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
